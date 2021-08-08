1bfdbc84f71990c92bf7920650b622d598d9ed71
Mangesh Pagar <mangeshpagar1@gmail.com>
2021-08-08 21:16:21 +0530

    Initial Commit

---
+++ src/com/goldmansachs/low/longestuniform/LongestUniformString.java
@@ -0,0 +1,10 @@
+package com.goldmansachs.low.longestuniform;
+
+public class LongestUniformString {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+    }
+}
