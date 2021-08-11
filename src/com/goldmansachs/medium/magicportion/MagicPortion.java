be0a8dc95fcd45dbbd09e9fb167f66ea88a36e08
Mangesh Pagar <mangeshpagar1@gmail.com>
2021-08-11 19:08:33 +0530

    Magic Portion

---
+++ src/com/goldmansachs/medium/magicportion/MagicPortion.java
@@ -0,0 +1,34 @@
+package com.goldmansachs.medium.magicportion;
+
+public class MagicPortion {
+
+    public  static  int magicPortion(String input){
+    StringBuilder magicPortion =  new StringBuilder();
+    magicPortion.append(input.charAt(0));
+
+    for (int i=1;i<input.length();i++){
+
+        if(i*2 <input.length()){
+             String left = input.substring(0,i);
+             if(left.equals(input.substring(i,i*2))){
+                 magicPortion.append("*");
+                 i = i * 2-1;
+             }else{
+                 magicPortion.append(input.charAt(i));
+             }
+
+        }else{
+            magicPortion.append(input.charAt(i));
+        }
+    }
+    return magicPortion.toString().length();
+    }
+
+    public static void main(String[] args) {
+        if (magicPortion("ABABCABABCE") == 6 && magicPortion("ABCABCE") == 5) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
