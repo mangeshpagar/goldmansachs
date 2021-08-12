0b8a5d8a08a6c462e2de2db8d7f167b13d37c4e3
Mangesh Pagar <mangeshpagar1@gmail.com>
2021-08-12 09:34:42 +0530

    Prime Factorization

---
+++ src/com/goldmansachs/low/primefactor/PrimeFactor.java
@@ -0,0 +1,51 @@
+package com.goldmansachs.low.primefactor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PrimeFactor {
+
+    public static ArrayList<Integer> primeFactorization(int no){
+        Set<Integer> primeFactor = new HashSet<>();
+        if(no<=1){
+            return new ArrayList<>();
+        }
+        if(no==2){
+            primeFactor.add(2);
+            return new ArrayList<>(primeFactor);
+        }
+
+        for (int i=2;i<Math.sqrt(no);i++){
+
+            while(no % i == 0){
+                primeFactor.add(i);
+                no/=i;
+            }
+
+        }
+
+        if(no > 1){
+            primeFactor.add(no);
+        }
+        return new ArrayList<>(primeFactor);
+
+    }
+
+    public static void main(String args[])
+    {
+
+        System.out.println(primeFactorization(42) + " " + primeFactorization(5));
+        if(primeFactorization(42).equals(Arrays.asList(2,3,7))
+                &&
+                primeFactorization(5).equals(Arrays.asList(5))
+        ) {
+            System.out.println("All passed");
+        }else {
+            System.out.println("Failed");
+        }
+
+    }
+
+}
