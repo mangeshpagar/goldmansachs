e3c113abfbec59251078027b0c3ac099aaebc326
Mangesh Pagar <mangeshpagar1@gmail.com>
2021-08-12 08:57:07 +0530

    is power of 10

---
+++ src/com/goldmansachs/low/power10/Power10.java
@@ -0,0 +1,46 @@
+package com.goldmansachs.low.power10;
+
+public class Power10 {
+
+    public static boolean isPowerOf10(int i){
+
+        while(i>1 && i%10 ==0){
+            i/=10;
+        }
+        return  i==1;
+
+    }
+
+    public static boolean doTestsPass()
+    {
+        int[] isPowerList = {10};
+        int[] isNotPowerList = {3};
+
+        for(int i : isPowerList)
+        {
+            if(!isPowerOf10(i))
+            {
+                System.out.println("Test failed for: " + i);
+                return false;
+            }
+        }
+
+        for(int i : isNotPowerList)
+        {
+            if(isPowerOf10(i))
+            {
+                System.out.println("Test failed for: " + i);
+                return false;
+            }
+        }
+
+        System.out.println("All tested passed");
+        return true;
+    };
+
+
+    public static void main(String args[])
+    {
+        doTestsPass();
+    }
+}
