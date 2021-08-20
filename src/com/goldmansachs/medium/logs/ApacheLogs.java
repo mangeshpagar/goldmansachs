490525cc0640207e528b482a9da1715fc9e468ce
Mangesh Pagar <mangeshpagar1@gmail.com>
2021-08-20 11:34:17 +0530

    Modified code

--- src/com/goldmansachs/medium/logs/ApacheLogs.java
+++ src/com/goldmansachs/medium/logs/ApacheLogs.java
@@ -3,6 +3,7 @@ package com.goldmansachs.medium.logs;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -25,14 +26,43 @@ public class ApacheLogs {
        return maxIpAddress;
     }
 
+    public static  String maxAccessedIpUsingStreamAndValidatingIp(String[] input){
+        if(null == input || input.length<1){
+            return "";
+        }
+
+        int maxFrequency = 0;
+        String maxIpAddress ="";
+        List<String> ips =   Stream.of(input).filter(line-> line != null).map(line-> line.split(" ")).
+                                map(line -> line[0]).filter(ApacheLogs::validateIp).collect(Collectors.toList());
+        for (String ip : ips) {
+            int frequency  = Collections.frequency(ips,ip);
+            if(frequency > maxFrequency){
+                maxFrequency = frequency;
+                maxIpAddress = ip;
+            }
+        }
+        return maxIpAddress;
+    }
+
+    public static boolean validateIp(String ip){
+        String reg = "(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
+        Pattern pattern= Pattern.compile(reg);
+        return pattern.matcher(ip).matches();
+    }
 
     public static void main(String[] args) {
 
         String lines[] = new String[] {
                 "10.0.0.1 - log entry 1 11",
                 "10.0.0.1 - log entry 2 213",
-                "10.0.0.2 - log entry 133132","abcd - xyz ","abcd - xyz ","abcd - xyz ","abcd - xyz " };
-        boolean result = maxAccessedIp(lines).equals("10.0.0.1");
+                "10.0.0.2 - log entry 133132",
+                "abcd - xyz ",
+                "abcd - xyz ",
+                    null,
+                "abcd - xyz ",
+                "abcd - xyz " };
+        boolean result = maxAccessedIpUsingStreamAndValidatingIp(lines).equals("10.0.0.1");
         if (result)
             System.out.println("Passed");
         else
